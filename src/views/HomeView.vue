990ca9a86197e3b8a2cd6b5b1922d2c08247fec5
周鹏 <peng.zhou8@amh-group.com>
2022-04-06 13:49:35 +0800

    initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <!-- <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer> -->
+      <el-form label-position="right" style="width: 500px" label-width="100px">
+        <el-form-item label="aaaa">
+          <el-input size="mini" />
+        </el-form-item>
+        <el-form-item label="bbbb">
+          <el-input size="mini" />
+        </el-form-item>
+      </el-form>
+    </el-container>
+  </el-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+  name: 'HomeView',
+  components: {},
+})
 </script>
